991a1233b7d590ce8bab996a8f338d11229dbcdb
Harvey Ana <harveyana3@gmail.com>
2023-10-12 03:10:40 +0100

    route reset

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGO_URL).then(()=>console.log('db connected')).ca
 
 app.use(express.json({limit:'10mb'}))
 app.use(express.urlencoded({limit:'10mb', extended: true}))
-// app.get('/', (req, res) => res.send('FurnitureSale Backend!'))
+app.get('/', (req, res) => res.send('FurnitureSale Backend!'))
 
 app.use(cors());
 
